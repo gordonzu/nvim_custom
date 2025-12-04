1577eff9a2114d443483d26ff643079b25857509
gordonzu <gordonzu@outlook.com>
2025-12-04 08:21:21 -0500

    Added treesitter. Conform next

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,31 @@
-if true then return {} end
+
+return {
+{
+	'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {
+			'bash',
+			'c',
+			'cpp',
+			'diff',
+			'html',
+			'lua',
+			'markdown',
+			'python',
+			'scala',
+			'go',
+			'vimdoc',
+		},
+    auto_install = true,
+    highlight = {
+    	enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+  },
+},
+}
+
+
